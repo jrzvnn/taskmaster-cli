abc3ba6580115805ad6b52fdb4fcfd260a3f340b
jrzvnn <jrzvnn@gmail.com>
2022-12-29 19:48:10 +0800

    Initial Commit

---
+++ todo.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Display the current to-do list
+python todo.py display
+
+echo "Enter a command (add, complete, display):"
+
+# Read a single line of input from the user
+read command
+
+case $command in
+  # If the user entered "add", ask for the item to add
+  add)
+    echo "Enter the item to add:"
+    read item
+    python todo.py add "$item"
+    ;;
+
+  # If the user entered "complete", ask for the item to mark as complete
+  complete)
+    echo "Enter the item to mark as complete:"
+    read item
+    python todo.py complete "$item"
+    ;;
+
+  # If the user entered "display", display the current to-do list
+  display)
+    python todo.py display
+    ;;
+
+  # If the user entered an invalid command, display an error message
+  *)
+    echo "Invalid command"
+    ;;
+esac
